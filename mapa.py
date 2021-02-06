a6c8a454c5b98a080f56d64afbc42c30d1eca5a0
chernyshev <serg.miass340@gmail.com>
2021-02-06 18:32:29 +0500

    zoom

--- mapa.py
+++ mapa.py
@@ -3,17 +3,26 @@ import requests
 import io
 
 size = WIDTH, HEIGHT = 650, 450
-
+KEYS = (pygame.K_PAGEDOWN, pygame.K_PAGEUP, pygame.K_DOWN,
+        pygame.K_UP, pygame.K_LEFT, pygame.K_RIGHT)
 url_static = "http://static-maps.yandex.ru/1.x/"
 
 
 class Map:
     def __init__(self, coord, zoom, layer="map", size=size):
-        self.lon, self.lat= coord
+        self.lon, self.lat = coord
         self.z = zoom
         self.layer = layer
         self.size = size
         self.update_map()
+
+    def update(self, event):
+        if event.key == pygame.K_PAGEDOWN:
+            self.z = max(0, self.z - 1)
+        if event.key == pygame.K_PAGEUP:
+            self.z = min(17, self.z + 1)
+        if event.key in KEYS:
+            self.update_map()
 
     def update_map(self):
         params = {"l": self.layer,
@@ -41,6 +50,8 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            mapapp.update(event)
 
     screen.blit(mapapp.map, (0, 0))
     pygame.display.flip()
